7e5e8f6e7f660937d201eab2f4d0dbbc36f99fd6
lossany <lossany08@gmail.com>
2024-08-11 10:32:12 +0700

    first commit

---
+++ connect/connect.php
@@ -0,0 +1,8 @@
+<?
+    $hostName = 'localhost';
+    $databaseName = 'poliklinik';
+    $username = 'root';
+    $password = '';
+
+    $mysqli = mysqli_connect($hostName, $databaseName, $username, $password);
+?>
---
+++ page/index.php
@@ -0,0 +1,3 @@
+<?php
+    include('./poliklinik/connect/connect.php')
+?>
